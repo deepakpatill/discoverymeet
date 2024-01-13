a626397ef6796f60de6b473c76ea06cd6484ccbe
Deepak Patil <deepak.patil@atriina.com>
2024-01-13 12:23:12 +0530

    feat: add github actions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
+import cron from "@elysiajs/cron";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().use(
+  cron({
+      name: 'DiscoveryMeet',
+      pattern: '*/30 7-21 * * *',
+      run() {
+        DiscoveryAutoCancel();
+          console.log('Heartbeat')
+      }
+  })
+)
+.listen(8080)
+
+async function DiscoveryAutoCancel(){
+  var response  = await fetch('https://eos.onefin.app/WeatherForecast/job/meeting/discovery-autocancel');
+  console.log(response.status);
+}
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
